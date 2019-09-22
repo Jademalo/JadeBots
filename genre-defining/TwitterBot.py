addbf440d213b5192df0b9c1c9d43b0f66215812
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-22 18:40:50 +0100

    Rewrite to use functions and general cleanup

--- genre-defining/TwitterBot.py
+++ genre-defining/TwitterBot.py
@@ -1,10 +1,9 @@
 import random, tweepy
 
-
 #-------------------------------------------------------------------------------
 # Random frequency variables
 
-postFreq = 1
+postFreq = 4
 altPostFreq = 12
 altGenreExtraFreq = 16
 altGenreGameFreq = 24
@@ -29,116 +28,87 @@ api = tweepy.API(auth)
 #-------------------------------------------------------------------------------
 # Opening the file and turning the games/genres into lists
 
-gameFile = open("game.txt", "r")
-gameString = gameFile.read()
-#print("gameString =", gameString)
-gameList = gameString.split("\n")
-#print("gameList =", gameList)
-gameList = list(filter(None, gameList))
-print("gameList =", gameList)
+def fileToList(fileName):
+    fileRaw = open(fileName, "r")
+    fileString = fileRaw.read()
+    fileList = fileString.split("\n")
+    return list(filter(None, fileList))
 
-genreFile = open("genre.txt", "r")
-genreString = genreFile.read()
-#print("genreString =", genreString)
-genreList = genreString.split("\n")
-#print("genreList =", genreList)
-genreList = list(filter(None, genreList))
-print("genreList =", genreList)
 
-genreExtraFile = open("genreExtra.txt", "r")
-genreExtraString = genreExtraFile.read()
-#print("genreExtraString =", genreExtraString)
-genreExtraList = genreExtraString.split("\n")
-#print("genreExtraList =", genreExtraList)
-genreExtraList = list(filter(None, genreExtraList))
-print("genreExtraList =", genreExtraList)
+gameList = fileToList("game.txt")
+genreList = fileToList("genre.txt")
+genreExtraList = fileToList("genreExtra.txt")
 
 
 #-------------------------------------------------------------------------------
-# Generating random values from the list
+# Functions to generate a random value and picking a word using that value
 
-gameNumber = len(gameList)
-#print("gameNumber =", gameNumber)
+def randomInt(freq):
+    return int(random.randint(1,freq))
 
-gamePick = int(random.randint(1,gameNumber))
-gamePick = gamePick - 1                                                         #One for zero being the first
-#print("gamePick =", gamePick)
-
-
-genreNumber = len(genreList)
-#print("genreNumber =", genreNumber)
-
-genrePick = int(random.randint(1,genreNumber))
-genrePick = genrePick - 1                                                       #One for zero being the first
-#print("genrePick =", genrePick)
-
-
-genreExtraNumber = len(genreExtraList)
-#print("genreExtraNumber =", genreExtraNumber)
-
-genreExtraPick = int(random.randint(1,genreExtraNumber))
-genreExtraPick = genreExtraPick - 1                                             #One for zero being the first
-#print("genreExtraPick =", genreExtraPick)
+def pickString(list):
+    listNumber = len(list)                                                      # Finding the length of the list
+    return list[randomInt(listNumber) - 1]                                      # Generating a random value from that list, -1 since 0 is a value # Returning the specific value from the list based on the number generated
 
 
 #-------------------------------------------------------------------------------
-# Picking a word using those values
+# Generating a Game and Genre from the lists
 
-gameText = gameList[gamePick]
-print("gameText =", gameText)
+gameText = pickString(gameList)
 
-altGenreExtra = int(random.randint(1,altGenreExtraFreq))
-#print("altGenreExtra =", altGenreExtra)
-altGenreGame = int(random.randint(1,altGenreGameFreq))
-#print("altGenreGame =", altGenreGame)
+
+altGenreGame = randomInt(altGenreGameFreq)
+altGenreGameDebug = ("No")
+altGenreExtra = randomInt(altGenreExtraFreq)
+altGenreExtraDebug = ("No")
+
 
 if altGenreGame == 1:
-    altGenreExtraDebug = ("No")
     altGenreGameDebug = ("Yes")
-    gamePickGenre = int(random.randint(2,gameNumber))
-    gamePickGenre = gamePickGenre - 2
-    gameTextGenre = gameList[gamePickGenre]
-    #print("gameTextGenre =", gameTextGenre)
-    workingGenre = (gameTextGenre, "like")
-    gamelikeGenre = "".join(workingGenre)
-    genreText = gamelikeGenre
+    genreText = pickString(gameList)
+    if genreText[-1:] == ("."):
+        genreText = genreText[:-1]                                              # If the last character is a ".", remove it
+    genreText = "".join((genreText, "like"))                                    # There are two brackets because the first set makes a string from the two values, and second joins them
 elif altGenreExtra == 1:
     altGenreExtraDebug = ("Yes")
-    altGenreGameDebug = ("No")
-    genreText = genreExtraList[genreExtraPick]
+    genreText = pickString(genreExtraList)
 else:
-    altGenreExtraDebug = ("No")
-    altGenreGameDebug = ("No")
-    genreText = genreList[genrePick]
+    genreText = pickString(genreList)
+
 
 print("Alternate Game as Genre? -", altGenreGameDebug)
 print("Alternate Extra Genre? -", altGenreExtraDebug)
+print("gameText =", gameText)
 print("genreText =", genreText)
 
 
 #-------------------------------------------------------------------------------
 # Working out of "a" or "an" is needed
+
 if genreText[0] == ("a") or genreText[0] == ("e") or genreText[0] == ("i") or genreText[0] == ("o") or genreText[0] == ("u") or genreText[0] == ("A") or genreText[0] == ("E") or genreText[0] == ("I") or genreText[0] == ("O") or genreText[0] == ("U"):
     connectText = (" an ")
 else:
     connectText = (" a ")
 
+
 #-------------------------------------------------------------------------------
 # Compositing the final tweet
 
 # Logic to decide if alternate format
-altPost = int(random.randint(1,altPostFreq))
-#print("altPost = ", altPost)
+altPost = randomInt(altPostFreq)
+altPostDebug = ("No")
+
 
 # If statement to change the format of the question, then composite the final tweet
 if altPost == 1:
     altPostDebug = ("Yes")
     workingTweet = ("What if ", gameText, " was", connectText, genreText, "?")
 else:
-    altPostDebug = ("No")
     workingTweet = ("Is ", gameText, connectText, genreText, "?")
 
 finalTweet = "".join(workingTweet)
+
+
 print("Alternate format? -", altPostDebug)
 print("finalTweet =", finalTweet)
 
@@ -146,13 +116,11 @@ print("finalTweet =", finalTweet)
 #-------------------------------------------------------------------------------
 # Posting the Tweet
 
-tweetPost = int(random.randint(1,postFreq))
-#print("tweetPost =", tweetPost)
+tweetPost = randomInt(postFreq)
+tweetPostDebug = ("No")
 
 if tweetPost == 1:
     tweetPostDebug = ("Yes")
-#    api.update_status(status = finalTweet)                                      #Update the Status
-else:
-    tweetPostDebug = ("No")
+#    api.update_status(status = finalTweet)                                     # Update the Status
 
 print("Post Tweet? -", tweetPostDebug)
