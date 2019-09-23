89c13eea45344bcb6e523648d5f0f99e2bba1071
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-23 02:47:45 +0100

    Fix indentation errors

--- JadeBotsRun.py
+++ JadeBotsRun.py
@@ -8,22 +8,30 @@ import JadeBots
 # Main Functions
 #-------------------------------------------------------------------------------
 
+#Genre Defining
+postFreq = 0
+altPostFreq = 12
+altGenreExtraFreq = 16
+altGenreGameFreq = 24
+verbose = False
+
 print("~Genre Defining~")
-    postFreq = 0
-    altPostFreq = 12
-    altGenreExtraFreq = 16
-    altGenreGameFreq = 24
-    verbose = False
 postGenreDefining(postFreq, altPostFreq, altGenreExtraFreq, altGenreGameFreq, verbose)
 
+
+# Romantics Ebooks
+postFreq = 0
+maxLength = 100
+print(maxLength)
+
 print("\n~Romantics eBooks~")
-    postFreq = 0
-    maxLength = 100
-    print(maxLength)
 postRomanticsEbooks(postFreq, maxLength)
 
+
+# Ulysses Ebooks
+postFreq = 0
+maxLength = 120
+print(maxLength)
+
 print("\n~Ulysses eBooks~")
-    postFreq = 0
-    maxLength = 120
-    print(maxLength)
 postUlyssesEbooks(postFreq, maxLength)
