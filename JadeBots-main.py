825f429e5c3d2ef7b8d81c0e6588cbc784af21bc
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-23 02:01:20 +0100

    Merge all of the individual accounts into a single file

---
+++ JadeBots-main.py
@@ -0,0 +1,74 @@
+#-------------------------------------------------------------------------------
+# Imports
+#-------------------------------------------------------------------------------
+import tweetBot
+
+
+#-------------------------------------------------------------------------------
+# Genre Defining
+#-------------------------------------------------------------------------------
+def postGenreDefining():
+
+    account = "genreDefining"
+    #postFreq = 4
+    postFreq = 0
+
+    altPostFreq = 12
+    altGenreExtraFreq = 16
+    altGenreGameFreq = 24
+
+    gameFile = "genre-defining/game.txt"
+    genreFile = "genre-defining/genre.txt"
+    genreExtraFile = "genre-defining/genreExtra.txt"
+
+    # Generate the tweet
+    tweetText = tweetBot.genreTweet(gameFile, genreFile, genreExtraFile, altPostFreq, altGenreGameFreq, altGenreExtraFreq)
+    print(tweetText)
+    # Post the tweet to Twitter
+    tweetBot.postTweet(tweetText, account, postFreq)
+
+
+
+#-------------------------------------------------------------------------------
+# Romantics eBooks
+#-------------------------------------------------------------------------------
+def postRomanticsEbooks():
+
+    account = "romanticsEbooks"
+    #postFreq = 15
+    postFreq = 0
+
+    mainFile = "romantics-ebooks/RomanticsText.txt"
+
+    # Generate the tweet
+    tweetText = tweetBot.ebooksTweet(mainFile)
+    print(tweetText)
+    # Post the tweet to Twitter
+    tweetBot.postTweet(tweetText, account, postFreq)
+
+
+
+#-------------------------------------------------------------------------------
+# Ulysses eBooks
+#-------------------------------------------------------------------------------
+def postUlyssesEbooks():
+
+    account = "ulyssesEbooks"
+    #postFreq = 15
+    postFreq = 0
+
+    mainFile = "ulysses-ebooks/Ulysses.txt"
+    # Generate the tweet
+    tweetText = tweetBot.ebooksTweet(mainFile)
+    print(tweetText)
+    # Post the tweet to Twitter
+    tweetBot.postTweet(tweetText, account, postFreq)
+
+
+
+#-------------------------------------------------------------------------------
+# Main Functions
+#-------------------------------------------------------------------------------
+postGenreDefining()
+postRomanticsEbooks()
+postUlyssesEbooks()
