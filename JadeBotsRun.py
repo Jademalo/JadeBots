3f9c9318f531b3c437270ea253f531174001cd2a
Jademalo <386846+Jademalo@users.noreply.github.com>
2019-09-23 02:49:40 +0100

    Final fix of the script so it will actually run

--- JadeBotsRun.py
+++ JadeBotsRun.py
@@ -9,7 +9,7 @@ import JadeBots
 #-------------------------------------------------------------------------------
 
 #Genre Defining
-postFreq = 0
+postFreq = 4
 altPostFreq = 12
 altGenreExtraFreq = 16
 altGenreGameFreq = 24
@@ -22,7 +22,6 @@ JadeBots.postGenreDefining(postFreq, altPostFreq, altGenreExtraFreq, altGenreGam
 # Romantics Ebooks
 postFreq = 0
 maxLength = 100
-print(maxLength)
 
 print("\n~Romantics eBooks~")
 JadeBots.postRomanticsEbooks(postFreq, maxLength)
@@ -31,7 +30,6 @@ JadeBots.postRomanticsEbooks(postFreq, maxLength)
 # Ulysses Ebooks
 postFreq = 0
 maxLength = 120
-print(maxLength)
 
 print("\n~Ulysses eBooks~")
 JadeBots.postUlyssesEbooks(postFreq, maxLength)
