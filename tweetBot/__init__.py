d2a498f7cbb876b7b74047349a857112dd3ad028
Jademalo <386846+Jademalo@users.noreply.github.com>
2021-04-13 10:22:31 +0100

    Remove tweetBot for submodule

--- tweetBot/__init__.py
+++
@@ -1,8 +0,0 @@
-import tweetBot.generator, tweetBot.poster
-
-# Add aliases when this module is imported
-tweetBot.ebooksGen = tweetBot.generator.ebooksGen
-tweetBot.genreGen = tweetBot.generator.genreGen
-tweetBot.variantGen = tweetBot.generator.variantGen
-
-tweetBot.postTweet = tweetBot.poster.postTweet
